97d434e6a0c7651ed9b2874cdb8ad387934fbdb0
aleksey.a <s1t3w4n@yandex.ru>
2025-07-04 10:59:32 +0300

    has refactored to reactive style

--- individuals_api/src/main/java/net/proselyte/individuals_api/api/AuthController.java
+++ individuals_api/src/main/java/net/proselyte/individuals_api/api/AuthController.java
@@ -2,17 +2,18 @@ package net.proselyte.individuals_api.api;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import net.proselyte.individuals_api.exception.AuthException;
 import net.proselyte.individuals_api.request.RefreshTokenRequest;
-import net.proselyte.individuals_api.response.ErrorResponse;
+import net.proselyte.individuals_api.response.AuthResponse;
 import net.proselyte.individuals_api.request.RegistrationRequest;
 import net.proselyte.individuals_api.request.LoginRequest;
+import net.proselyte.individuals_api.response.UserInfoResponse;
 import net.proselyte.individuals_api.service.KeycloakService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/v1/auth")
@@ -22,63 +23,23 @@ public class AuthController {
     private final KeycloakService keycloakService;
 
     @PostMapping("/registration")
-    public ResponseEntity<?> registerUser(@Valid @RequestBody RegistrationRequest request) {
-        if (Boolean.FALSE.equals(request.password().equals(request.confirmPassword()))) {
-            return ResponseEntity.badRequest()
-                    .body(new ErrorResponse("Password confirmation does not match", HttpStatus.BAD_REQUEST.value()));
-        }
-
-        try {
-            var authResponse = keycloakService.registerUser(request);
-            return ResponseEntity.status(HttpStatus.CREATED).body(authResponse);
-        } catch (AuthException ex) {
-            return ResponseEntity.status(ex.getStatus())
-                    .body(new ErrorResponse(ex.getMessage(), ex.getStatus().value()));
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError()
-                    .body(new ErrorResponse("Registration failed: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        }
+    public Mono<ResponseEntity<AuthResponse>> registerUser(@Valid @RequestBody RegistrationRequest request) {
+        return Mono.just(request).flatMap(req -> keycloakService.registerUser(req)
+                .map(authResponse -> ResponseEntity.status(HttpStatus.CREATED).body(authResponse)));
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@Valid @RequestBody LoginRequest request) {
-        try {
-            var authResponse = keycloakService.loginUser(request.email(), request.password());
-            return ResponseEntity.ok(authResponse);
-        } catch (AuthException ex) {
-            return ResponseEntity.status(ex.getStatus())
-                    .body(new ErrorResponse(ex.getMessage(), ex.getStatus().value()));
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError()
-                    .body(new ErrorResponse("Login failed: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        }
+    public Mono<AuthResponse> loginUser(@Valid @RequestBody Mono<LoginRequest> requestMono) {
+        return requestMono.flatMap(request -> keycloakService.loginUser(request.email(), request.password()));
     }
 
     @PostMapping("/refresh-token")
-    public ResponseEntity<?> refreshToken(@Valid @RequestBody RefreshTokenRequest request) {
-        try {
-            var authResponse = keycloakService.refreshToken(request.refreshToken());
-            return ResponseEntity.ok(authResponse);
-        } catch (AuthException ex) {
-            return ResponseEntity.status(ex.getStatus())
-                    .body(new ErrorResponse(ex.getMessage(), ex.getStatus().value()));
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError()
-                    .body(new ErrorResponse("Token refresh failed: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        }
+    public Mono<AuthResponse> refreshToken(@Valid @RequestBody RefreshTokenRequest request) {
+        return keycloakService.refreshToken(request.refreshToken());
     }
 
     @GetMapping("/me")
-    public ResponseEntity<?> getUserInfo(@AuthenticationPrincipal Jwt jwt) {
-        try {
-            var userInfo = keycloakService.getUserInfo(jwt);
-            return ResponseEntity.ok(userInfo);
-        } catch (AuthException ex) {
-            return ResponseEntity.status(ex.getStatus())
-                    .body(new ErrorResponse(ex.getMessage(), ex.getStatus().value()));
-        } catch (Exception e) {
-            return ResponseEntity.internalServerError()
-                    .body(new ErrorResponse("User get info failed: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value()));
-        }
+    public Mono<UserInfoResponse> getUserInfo(@AuthenticationPrincipal Jwt jwt) {
+        return keycloakService.getUserInfo(jwt);
     }
 }
--- individuals_api/src/main/java/net/proselyte/individuals_api/service/KeycloakService.java
+++ individuals_api/src/main/java/net/proselyte/individuals_api/service/KeycloakService.java
@@ -1,9 +1,6 @@
 package net.proselyte.individuals_api.service;
 
 import jakarta.ws.rs.NotAuthorizedException;
-import jakarta.ws.rs.client.ClientBuilder;
-import jakarta.ws.rs.client.Entity;
-import jakarta.ws.rs.core.Form;
 import jakarta.ws.rs.core.Response;
 import lombok.RequiredArgsConstructor;
 import net.proselyte.individuals_api.exception.AuthException;
@@ -11,9 +8,8 @@ import net.proselyte.individuals_api.response.AuthResponse;
 import net.proselyte.individuals_api.request.RegistrationRequest;
 import net.proselyte.individuals_api.response.UserInfoResponse;
 import org.keycloak.OAuth2Constants;
+import org.keycloak.admin.client.Keycloak;
 import org.keycloak.admin.client.KeycloakBuilder;
-import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.representations.AccessTokenResponse;
 import org.keycloak.representations.idm.CredentialRepresentation;
 import org.keycloak.representations.idm.UserRepresentation;
@@ -21,7 +17,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
+import java.time.Instant;
 import java.util.*;
 
 @Component
@@ -46,116 +49,146 @@ public class KeycloakService {
     @Value("${keycloak.admin.password}")
     private String adminPassword;
 
-    public AuthResponse registerUser(RegistrationRequest request) {
-        createUserInKeycloak(request);
-        return loginUser(request.email(), request.password());
+    public Mono<AuthResponse> registerUser(RegistrationRequest request) {
+        return createUserInKeycloak(request)
+                .then(loginUser(request.email(), request.password()));
     }
 
-    private void createUserInKeycloak(RegistrationRequest request) {
-        try (var keycloakAdmin = KeycloakBuilder.builder()
-                .serverUrl(authServerUrl)
-                .realm(realm)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .username(adminUsername)
-                .password(adminPassword)
-                .build()) {
+    private Mono<Void> createUserInKeycloak(RegistrationRequest request) {
+        return Mono.using(
+                () -> KeycloakBuilder.builder()
+                        .serverUrl(authServerUrl)
+                        .realm(realm)
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .username(adminUsername)
+                        .password(adminPassword)
+                        .build(),
+                keycloakAdmin -> Mono.fromCallable(() -> {
+                    UserRepresentation user = getUserRepresentation(request);
+                    try (Response response = keycloakAdmin.realm(realm).users().create(user)) {
+                        if (response.getStatus() == HttpStatus.CONFLICT.value()) {
+                            throw new AuthException("User with this email already exists", HttpStatus.CONFLICT);
+                        }
+                        if (response.getStatus() != HttpStatus.CREATED.value()) {
+                            throw new AuthException("Failed to create user", HttpStatus.INTERNAL_SERVER_ERROR);
+                        }
+                    }
+                    return null;
+                }),
+                keycloak -> Optional.ofNullable(keycloak).ifPresent(Keycloak::close)
+        );
+    }
 
-            var user = new UserRepresentation();
-            user.setEnabled(true);
-            user.setUsername(request.email());
-            user.setEmail(request.email());
-            user.setEmailVerified(true);
+    private static UserRepresentation getUserRepresentation(RegistrationRequest request) {
+        UserRepresentation user = new UserRepresentation();
+        user.setEnabled(true);
+        user.setUsername(request.email());
+        user.setEmail(request.email());
+        user.setEmailVerified(true);
 
-            var credential = new CredentialRepresentation();
-            credential.setType(CredentialRepresentation.PASSWORD);
-            credential.setValue(request.password());
-            credential.setTemporary(false);
+        CredentialRepresentation credential = new CredentialRepresentation();
+        credential.setType(CredentialRepresentation.PASSWORD);
+        credential.setValue(request.password());
+        credential.setTemporary(false);
+        user.setCredentials(List.of(credential));
+        return user;
+    }
 
-            user.setCredentials(List.of(credential));
+    public Mono<AuthResponse> loginUser(String username, String password) {
+        return Mono.fromCallable(() -> {
+                    try (var keycloak = KeycloakBuilder.builder()
+                            .serverUrl(authServerUrl)
+                            .realm(realm)
+                            .clientId(clientId)
+                            .clientSecret(clientSecret)
+                            .username(username)
+                            .password(password)
+                            .build()) {
+                        var tokenResponse = keycloak.tokenManager().getAccessToken();
+                        return new AuthResponse(
+                                tokenResponse.getToken(),
+                                tokenResponse.getExpiresIn(),
+                                tokenResponse.getRefreshToken(),
+                                tokenResponse.getTokenType()
+                        );
+                    }
+                })
+                .onErrorResume(NotAuthorizedException.class, ex ->
+                        Mono.error(new AuthException("Invalid email or password", HttpStatus.UNAUTHORIZED)));
+    }
 
-            var realmResource = keycloakAdmin.realm(realm);
-            var usersResource = realmResource.users();
+    public Mono<AuthResponse> refreshToken(String refreshToken) {
+        return WebClient.builder()
+                .baseUrl(authServerUrl)
+                .defaultHeader("Content-Type", "application/x-www-form-urlencoded")
+                .build()
+                .post()
+                .uri("/realms/" + realm + "/protocol/openid-connect/token")
+                .body(BodyInserters.fromFormData(createFormData(refreshToken)))
+                .retrieve()
+                .bodyToMono(AccessTokenResponse.class)
+                .map(this::convertToAuthResponse)
+                .onErrorResume(this::handleTokenRefreshError);
+    }
 
-            try (Response response = usersResource.create(user)) {
-                if (response.getStatus() == HttpStatus.CONFLICT.value()) {
-                    throw new AuthException("User with this email already exists", HttpStatus.CONFLICT);
-                }
-                if (response.getStatus() != HttpStatus.CREATED.value()) {
-                    throw new AuthException("Failed to create user", HttpStatus.INTERNAL_SERVER_ERROR);
-                }
-            }
+    private MultiValueMap<String, String> createFormData(String refreshToken) {
+        MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
+        formData.add(OAuth2Constants.GRANT_TYPE, OAuth2Constants.REFRESH_TOKEN);
+        formData.add(OAuth2Constants.CLIENT_ID, clientId);
+        formData.add(OAuth2Constants.CLIENT_SECRET, clientSecret);
+        formData.add(OAuth2Constants.REFRESH_TOKEN, refreshToken);
+        return formData;
+    }
+
+    private AuthResponse convertToAuthResponse(AccessTokenResponse response) {
+        return new AuthResponse(
+                response.getToken(),
+                response.getExpiresIn(),
+                response.getRefreshToken(),
+                response.getTokenType()
+        );
+    }
+
+    private Mono<AuthResponse> handleTokenRefreshError(Throwable ex) {
+        if (ex instanceof WebClientResponseException) {
+            return Mono.error(new AuthException("Invalid or expired refresh token", HttpStatus.UNAUTHORIZED));
         }
+        return Mono.error(new AuthException(
+                "Token refresh failed: " + ex.getMessage(),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        ));
     }
 
-    public AuthResponse loginUser(String username, String password) {
-        try (var keycloak = KeycloakBuilder.builder()
-                .serverUrl(authServerUrl)
-                .realm(realm)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .username(username)
-                .password(password)
-                .build()) {
-            var tokenResponse = keycloak.tokenManager().getAccessToken();
-            return new AuthResponse(
-                    tokenResponse.getToken(),
-                    tokenResponse.getExpiresIn(),
-                    tokenResponse.getRefreshToken(),
-                    tokenResponse.getTokenType()
-            );
-        } catch (NotAuthorizedException ex) {
-            throw new AuthException("Invalid email or password", HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-    public AuthResponse refreshToken(String refreshToken) {
-        try (var client = ClientBuilder.newClient()) {
-            var form = new Form()
-                    .param(OAuth2Constants.GRANT_TYPE, OAuth2Constants.REFRESH_TOKEN)
-                    .param(OAuth2Constants.CLIENT_ID, clientId)
-                    .param(OAuth2Constants.CLIENT_SECRET, clientSecret)
-                    .param(OAuth2Constants.REFRESH_TOKEN, refreshToken);
-            var tokenResponse = client.target(authServerUrl + "/realms/" + realm + "/protocol/openid-connect/token")
-                    .request()
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .post(Entity.form(form), AccessTokenResponse.class);
-            return new AuthResponse(
-                    tokenResponse.getToken(),
-                    tokenResponse.getExpiresIn(),
-                    tokenResponse.getRefreshToken(),
-                    tokenResponse.getTokenType()
-            );
-        } catch (Exception ex) {
-            throw new AuthException("Invalid or expired refresh token", HttpStatus.UNAUTHORIZED);
-        }
-    }
-
-    public UserInfoResponse getUserInfo(Jwt jwt) {
+    public Mono<UserInfoResponse> getUserInfo(Jwt jwt) {
         String userId = jwt.getSubject();
         String email = jwt.getClaim("email");
-
-        try (var keycloak = KeycloakBuilder.builder()
-                .serverUrl(authServerUrl)
-                .realm(realm)
-                .clientId(clientId)
-                .clientSecret(clientSecret)
-                .username(adminUsername)
-                .password(adminPassword)
-                .build()) {
-            RealmResource realmResource = keycloak.realm(realm);
-            UserResource userResource = realmResource.users().get(userId);
-            UserRepresentation user = userResource.toRepresentation();
-            if (Objects.isNull(user)) {
-                throw new AuthException("User not found", HttpStatus.NOT_FOUND);
-            }
-        }
-        return new UserInfoResponse(
+        List<String> roles = extractRoles(jwt);
+        Instant iat = jwt.getClaim("iat");
+        return Mono.using(
+                () -> KeycloakBuilder.builder()
+                        .serverUrl(authServerUrl)
+                        .realm(realm)
+                        .clientId(clientId)
+                        .clientSecret(clientSecret)
+                        .username(adminUsername)
+                        .password(adminPassword)
+                        .build(),
+                keycloak -> Mono.fromCallable(() -> {
+                    UserRepresentation user = keycloak.realm(realm)
+                            .users()
+                            .get(userId)
+                            .toRepresentation();
+                    return Optional.ofNullable(user)
+                            .orElseThrow(() -> new AuthException("User not found", HttpStatus.NOT_FOUND));
+                }),
+                keycloak -> Optional.ofNullable(keycloak).ifPresent(Keycloak::close)
+        ).thenReturn(new UserInfoResponse(
                 userId,
                 email,
-                extractRoles(jwt),
-                jwt.getClaim("iat")
-        );
+                roles,
+                iat
+        )).onErrorResume(AuthException.class, Mono::error);
     }
 
     private List<String> extractRoles(Jwt jwt) {
