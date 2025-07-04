b08ea112d3cfc3346f46c9cd9f4498266b33dcba
aleksey.a <s1t3w4n@yandex.ru>
2025-07-04 12:19:03 +0300

    has refactored GlobalErrorHandler using ErrorResponse

--- individuals_api/src/main/java/net/proselyte/individuals_api/errorhandling/GlobalErrorHandler.java
+++ individuals_api/src/main/java/net/proselyte/individuals_api/errorhandling/GlobalErrorHandler.java
@@ -1,6 +1,7 @@
 package net.proselyte.individuals_api.errorhandling;
 
 import net.proselyte.individuals_api.exception.AuthException;
+import net.proselyte.individuals_api.response.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -8,23 +9,19 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.reactive.result.method.annotation.ResponseEntityExceptionHandler;
 import reactor.core.publisher.Mono;
 
-import java.util.Map;
-
 @ControllerAdvice
 public class GlobalErrorHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler
     public Mono<ResponseEntity<?>> handleBusinessException(AuthException ex) {
         return Mono.just(ResponseEntity
                 .status(ex.getStatus())
-                .body(Map.of("error", ex.getMessage(), "status", ex.getStatus().value())));
+                .body(new ErrorResponse(ex.getMessage(), ex.getStatus().value())));
     }
 
     @ExceptionHandler
     public Mono<ResponseEntity<?>> handleAllExceptions(Exception ex) {
         return Mono.just(ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(Map.of(
-                        "error", "Unexpected server error: " + ex.getMessage(),
-                        "status", HttpStatus.INTERNAL_SERVER_ERROR.value())));
+                .body(new ErrorResponse("Unexpected server error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value())));
     }
 }
